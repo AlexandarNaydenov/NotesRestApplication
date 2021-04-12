84773e0521b8e105dcdfd9e3fac65c216791be6d
Alexandar Naydenov <alexandar.naydenov@sap.com>
2021-04-12 13:59:02 +0300

    Spring Init Commit

---
+++ src/test/java/com/sap/NotesRestApplication/NotesRestApplication/NotesRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.NotesRestApplication.NotesRestApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
