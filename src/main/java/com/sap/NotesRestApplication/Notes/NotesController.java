a768ce0ac6bcad476e21b0e3f7116e3e8eba5c45
Alexandar Naydenov <alexandar.naydenov@sap.com>
2021-04-13 15:51:18 +0300

    Fixing Responses from Controller

--- src/main/java/com/sap/NotesRestApplication/Notes/NotesController.java
+++ src/main/java/com/sap/NotesRestApplication/Notes/NotesController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.management.InstanceNotFoundException;
-import java.util.List;
 import java.util.Map;
 
 @RestController
